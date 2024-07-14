88253717568d63eb49b36b31e4d32b383ee01ffd
ij2002 <ij2002@hw.ac.uk>
2024-07-14 21:32:14 +0530

    completed the task and updated css

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +23,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      //show graph later when button is clicked
+      showGraph: false,
     };
   }
 
@@ -29,18 +32,34 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    //if showGraph is true, render the graph
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
+    //get data from it continuously
+    let requestCount = 0;
+    let interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+      // this.setState({ data: [...this.state.data, ...serverResponds] });
+      this.setState({ 
+        data: serverResponds,
+        showGraph: true,
+      });
+      //clear the interval if no more data is returned or if requestCount is greater than 1000
+      if (serverResponds.length === 0 || requestCount > 1000) {
+        clearInterval(interval);
+      }
+      requestCount++;
     });
+    }, 100);
   }
 
   /**
@@ -53,7 +72,8 @@ class App extends Component<{}, IState> {
           Bank & Merge Co Task 2
         </header>
         <div className="App-content">
-          <button className="btn btn-primary Stream-button"
+          {/* <button className="btn btn-primary Stream-button" */}
+          <button className="stream-button"
             // when button is click, our react app tries to request
             // new data from the server.
             // As part of your task, update the getDataFromServer() function
@@ -62,7 +82,8 @@ class App extends Component<{}, IState> {
             onClick={() => {this.getDataFromServer()}}>
             Start Streaming Data
           </button>
-          <div className="Graph">
+          {/* <div className="Graph"> */}
+          <div className="graph-container">
             {this.renderGraph()}
           </div>
         </div>
